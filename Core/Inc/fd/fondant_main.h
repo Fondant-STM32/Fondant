8aeddc67846e6b9d1c91613fac7db55090036de8
Niels Post <niels.post.97@gmail.com>
2022-06-24 13:38:54 +0200

    - Developmentbestanden weggehaald - Licentie toegevoegd

--- Core/Inc/fd/fondant_main.h
+++
@@ -1,7 +0,0 @@
-#ifndef FD_HDR_FONDANT_MAIN_H
-#define FD_HDR_FONDANT_MAIN_H
-
-extern int fd_main_caller();
-
-#endif //FD_HDR_FONDANT_MAIN_H
-
--- Core/Src/fd_src/targets/base/gpio.cpp
+++
@@ -1,4 +0,0 @@
-#include <fd/targets/base/gpio.hpp>
-
-fd::pin_out_base fd::pin_out_dummy;
-fd::pin_in_base fd::pin_in_dummy;
